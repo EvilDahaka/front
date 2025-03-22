4205d66656e4c2253327792da56f07218b1c4841
EvilDahaka <161943190+EvilDahaka@users.noreply.github.com>
2025-03-22 18:02:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,44 @@
+from flask import Flask, render_template, session
+#from models import init_db
+#from routes.feedback import feedback_bp
+#from routes.products import products_bp
+#from routes.admin import admin_bp
+#from routes.api import api_bp
+#from routes.user import user_bp, auth, get_username
+
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+#app.register_blueprint(admin_bp)
+#app.register_blueprint(feedback_bp)
+#app.register_blueprint(products_bp)
+#app.register_blueprint(api_bp)
+#app.register_blueprint(user_bp)
+
+
+
+
+# Ініціалізація бази даних
+
+#init_db()
+#перевірка аутентифікація користувача?
+#@app.context_processor
+#def inject_auth():
+    #return {'auth': auth(),'username':get_username()}
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('test.html')
+'''@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+'''
+if __name__ == '__main__':
+    app.run(debug=True)
 
