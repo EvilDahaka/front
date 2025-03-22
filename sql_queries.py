30ec1a99431a76f88c39b6d96e77cdfa28a3ff36
EvilDahaka <161943190+EvilDahaka@users.noreply.github.com>
2025-03-22 18:26:30 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -2,18 +2,36 @@ import sqlite3
 
 conn = sqlite3.connect('Artistc.db')
 cursor = conn.cursor()
-cursor.execute('''SELECT * FROM Artistc''')
+cursor.execute('''SELECT * FROM artists''')
 data = cursor.fetchall()
 conn.commit()
 
 
+
 # Запитання 1. Інформація про скількох художників представлена у базі даних?
+cursor.execute('''SELECT COUNT(*) FROM artists;''')
+data = cursor.fetchall()
+conn.commit()
+print(data)
+
 
 
 # Запитання 2. Скільки жінок (Female) у базі?
+cursor.execute('''SELECT * FROM artists WHERE Gender == 2''')
+data = cursor.fetchall()
+conn.commit()
+print(data)
+
 
 
 # Запитання 3. Скільки людей у базі даних народилися до 1900 року?
+#cursor.execute('''SELECT * FROM artists WHERE Birth Year <1900''')
+#data = cursor.fetchall()
+#conn.commit()
 
 
 # Запитання 4*. Як звати найстаршого художника?
+cursor.execute('''SELECT Name FROM artists ORDER BY "Birth Year"''')
+data = cursor.fetchone()
+conn.commit()
+print(data)
