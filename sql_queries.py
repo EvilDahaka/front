adba7b78dae4d867143f88dd45c2857936d73f0f
EvilDahaka <davidlesho100608@gmail.com>
2025-03-26 12:21:58 +0200

    up

--- sql_queries.py
+++ sql_queries.py
@@ -17,7 +17,7 @@ print(data)
 
 
 # Запитання 2. Скільки жінок (Female) у базі?
-cursor.execute('''SELECT * FROM artists WHERE Gender == 2''')
+cursor.execute('SELECT COUNT(*) FROM artists WHERE Gender = "Female";')
 data = cursor.fetchall()
 conn.commit()
 print(data)
@@ -25,9 +25,9 @@ print(data)
 
 
 # Запитання 3. Скільки людей у базі даних народилися до 1900 року?
-#cursor.execute('''SELECT * FROM artists WHERE Birth Year <1900''')
-#data = cursor.fetchall()
-#conn.commit()
+cursor.execute('SELECT COUNT(*) FROM artists WHERE "Birth Year" < 1900;')
+data = cursor.fetchall()
+conn.commit()
 
 
 # Запитання 4*. Як звати найстаршого художника?
